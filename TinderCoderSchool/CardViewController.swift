b11d3fb40cfe2b74045ab7c63da01583272a518f
Charles Lee <chug2k@gmail.com>
2016-07-25 19:22:27 +0700

    Milestone 1

---
+++ TinderCoderSchool/CardViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderCoderSchool
+//
+//  Created by Charles Lee on 7/25/16.
+//  Copyright © 2016 coderschoolvn. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
